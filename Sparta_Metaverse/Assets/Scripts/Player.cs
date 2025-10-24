144bbd9d4c6a7f5e72f8c6ed0adf5273a63de624
kuminji125-ui <kuminji125@gmail.com>
2025-10-24 17:38:47 +0900

    feat: 마우스로도 이동 가능

--- Sparta_Metaverse/Assets/Scripts/Player.cs
+++ Sparta_Metaverse/Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     public float moveSpeed = 4.0f; // 플레이어 속도
     float h;
     float v;
+    private bool isMovingToMouse = false;
+    private Vector3 targetPosition;
 
     Rigidbody2D rigid;
 
@@ -18,10 +20,37 @@ public class Player : MonoBehaviour
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.z = transform.position.z;
+            isMovingToMouse = true;
+        }
+        if (h != 0 || v != 0)
+        {
+            isMovingToMouse = false;
+        }
     }
 
     private void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h * moveSpeed, v * moveSpeed);
+        if (isMovingToMouse)
+        {
+            Vector2 direction = targetPosition - transform.position;
+            if(direction.magnitude < 0.1f)
+            {
+                isMovingToMouse = false;
+                rigid.velocity = Vector2.zero;
+            }
+            else
+            {
+                direction.Normalize();
+                rigid.velocity = direction * moveSpeed;
+            }
+        }
+        else
+        {
+            rigid.velocity = new Vector2(h * moveSpeed, v * moveSpeed);
+        }     
     }
 }
