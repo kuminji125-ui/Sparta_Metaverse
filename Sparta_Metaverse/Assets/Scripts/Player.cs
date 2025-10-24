ff5a26e7f8b6db2955d0fe1794a8fe97bdee577f
kuminji125-ui <kuminji125@gmail.com>
2025-10-24 19:57:41 +0900

    feat: 이동 애니메이션 구현
    키보드와 마우스 클릭 방향으로 캐릭터가 이동하는 애니메이션 구현

--- Sparta_Metaverse/Assets/Scripts/Player.cs
+++ Sparta_Metaverse/Assets/Scripts/Player.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 4.0f; // 플레이어 속도
+    public float moveSpeed = 3.0f; // 플레이어 속도
     float h;
     float v;
     private bool isMovingToMouse = false;
     private Vector3 targetPosition;
-
+    Animator anim;
     Rigidbody2D rigid;
 
     private void Awake()
     {
+        anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
     }
     private void Update()
@@ -26,9 +28,79 @@ public class Player : MonoBehaviour
             targetPosition.z = transform.position.z;
             isMovingToMouse = true;
         }
-        if (h != 0 || v != 0)
+        if(h!=0 || v != 0)
         {
             isMovingToMouse = false;
+        }
+        Vector2 currentInput = Vector2.zero;
+        bool isMoving = false;
+        if(h!=0 || v != 0)
+        {
+            currentInput = new Vector2(h, v);
+            isMoving = true;
+        }else if (isMovingToMouse)
+        {
+            Vector2 directionToTarget = targetPosition - transform.position;
+            if(directionToTarget.magnitude < 0.1f)
+            {
+                isMovingToMouse = false;
+            }
+            else
+            {
+                currentInput = directionToTarget.normalized;
+                isMoving = true;
+            }
+        }
+        int animH = 0;
+        int animV = 0;
+        if (isMoving)
+        {
+            float absH = Mathf.Abs(currentInput.x);
+            float absV = Mathf.Abs(currentInput.y);
+            if (absH > absV) { animH = (int)Mathf.Sign(currentInput.x); }
+            else if (absV > 0) { animV = (int)Mathf.Sign(currentInput.y); }
+        }
+        bool hChange = anim.GetInteger("hAxisRaw") != animH;
+        bool vChange = anim.GetInteger("vAxisRaw") != animV;
+        if (isMoving)
+        {
+            if (hChange || vChange)
+            {
+                anim.SetBool("isChange", true);
+            }
+            else
+            {
+                anim.SetBool("isChange", false);
+            }
+            anim.SetInteger("hAxisRaw", animH);
+            anim.SetInteger("vAxisRaw", animV);
+        }
+        else
+        {
+            if (anim.GetInteger("hAxisRaw") != 0 || anim.GetInteger("vAxisRaw") != 0)
+            {
+                anim.SetBool("isChange", true);
+                anim.SetInteger("hAxisRaw", animH);
+                anim.SetInteger("vAxisRaw", animV);
+            }
+            else
+            {
+                anim.SetBool("isChange", false);
+            }
+            if (anim.GetInteger("hAxisRaw") != (int)h)
+            {
+                anim.SetBool("isChange", true);
+                anim.SetInteger("hAxisRaw", (int)h);
+            }
+            else if (anim.GetInteger("vAxisRaw") != (int)v)
+            {
+                anim.SetBool("isChange", true);
+                anim.SetInteger("vAxisRaw", (int)v);
+            }
+            else
+            {
+                anim.SetBool("isChange", false);
+            }
         }
     }
 
